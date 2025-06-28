e47c8b9dea1e686387799b34b23312851a31125f
Tathya <coding.tathya@gmail.com>
2025-06-28 09:54:47 +0530

    doc: Added information about syntax

--- src/main.zig
+++ src/main.zig
@@ -7,10 +7,10 @@ pub fn main() !void {
     var parser = cli.Parser.init("my_program", "A simple CLI program", std.heap.page_allocator);
 
     try parser.add_argument(@constCast(&cli.Argument{
-        .name = "output",
-        .description = "The output file to write to",
-        .option = "--out",
-        .flag = "-O",
+        .name = "file",
+        .description = "The file to interpret",
+        .option = "--file",
+        .flag = "-F",
         .positional = true,
     }));
 
@@ -32,6 +32,6 @@ pub fn main() !void {
     };
 
     std.debug.print("Parsed arguments successfully.\n", .{});
-    std.debug.print("Output file: {s}\n", .{parser.arguments.get("output").?.value orelse "not specified"});
+    std.debug.print("Input file: {s}\n", .{parser.arguments.get("file").?.value orelse "none"});
     std.debug.print("Verbose mode: {s}\n", .{parser.arguments.get("verbose").?.value orelse "off"});
 }
